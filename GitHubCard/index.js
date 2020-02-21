d8a879c1a0575334c12a3477a5398efddfa64fbb
Bradley Drummonds <47236494+bdrummo6@users.noreply.github.com>
2020-02-20 20:19:15 -0500

    Update index.js

--- GitHubCard/index.js
+++ GitHubCard/index.js
@@ -3,6 +3,8 @@
            https://api.github.com/users/<your name>
 */
 
+axios.get("https://api.github.com/users/bdrummo6");
+
 /* Step 2: Inspect and study the data coming back, this is YOUR 
    github info! You will need to understand the structure of this 
    data in order to use it to build your component function 
@@ -46,10 +48,79 @@ const followersArray = [];
 
 */
 
+// Get the 'div' cards for which all gitcards will be appended to
+const cards = document.querySelector('.cards');
+
+const createCard = (obj) => {
+   // Creates the 'div' with the class of 'card' this is the card component
+   const card = document.createElement('div');
+   card.classList.add('card');
+
+   // Creates an 'img' element that will hold the url for the user's image
+   const img = document.createElement('img');
+   img.src = obj.avatar_url;
+
+   // Creates a 'div' with the class 'card-info'
+   const cardInfo = document.createElement('div');
+   cardInfo.classList.add('card-info');
+
+   // Creates a 'h3' element for the user's full name with the class 'name'
+   const name = document.createElement('h3');
+   name.classList.add('name');
+   name.textContent = obj.name;
+
+   // Creates a 'p' element for holding the username with the class 'username'
+   const username = document.createElement('p');
+   username.classList.add('username');
+   username.textContent = obj.login;
+
+   // Creates a 'p' element for holding the location
+   const location = document.createElement('p');
+   location.textContent = `Location: ${obj.location}`;
+
+   // Creates a 'p' element for holding the profile
+   const profile = document.createElement('p');
+   profile.textContent = 'Profile: ';
+
+   // Creates an 'a' element for holding the profile url
+   const profileURL = document.createElement('a');
+   profileURL.setAttribute('href', obj.html_url);
+   profileURL.textContent = obj.html_url;
+
+   // Creates a 'p' element for holding the followers
+   const followers = document.createElement('p');
+   followers.textContent = `Followers: ${obj.followers}`;
+
+   // Creates a 'p' element for holding the following
+   const following = document.createElement('p');
+   following.textContent = `Following: ${obj.following}`;
+
+   // Creates an 'a' element for holding the profile bio
+   const bio = document.createElement('p');
+   bio.textContent = `Bio: ${obj.bio}`;
+
+   // Append img and cardInfo to card
+   card.appendChild(img);
+   card.appendChild(cardInfo);
+
+   // Append name, username, location and profile to cardInfo
+   cardInfo.appendChild(name);
+   cardInfo.appendChild(username);
+   cardInfo.appendChild(location);
+   cardInfo.appendChild(profile);
+
+   // Append the profile URL to profile
+   profile.appendChild(profileURL);
+
+   // Append followers, following and bio to cardInfo
+   cardInfo.appendChild(followers);
+   cardInfo.appendChild(following);
+   cardInfo.appendChild(bio);
+
+   return card; // returns card component
+
+};
+
 /* List of LS Instructors Github username's: 
-  tetondan
-  dustinmyers
-  justsml
-  luishrd
-  bigknell
+
 */
